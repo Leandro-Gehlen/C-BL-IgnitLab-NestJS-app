de7515052f3b824864634d9be18a70fffaa24b70
Leandro Gehlen <legedeveloper@gmail.com>
2023-03-29 16:00:58 -0300

    excluding testing docs and starting config typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+
+
 @Module({
-  imports: [],
+  imports: [
+
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+
+    AppService
+  ],
 })
-export class AppModule {}
+export class AppModule { }
